6c754d529a7502192feaad7f1fc7589d384c6613
Eiji Toriki <ageForEngi@outlook.jp>
2023-12-21 18:45:37 +0900

    POST, PUT, DELETE  APIの講義受講

--- spring_rest/src/main/java/com/udemy_rest/spring_rest/controller/ItemController.java
+++ spring_rest/src/main/java/com/udemy_rest/spring_rest/controller/ItemController.java
@@ -3,10 +3,7 @@ package com.udemy_rest.spring_rest.controller;
 import com.udemy_rest.spring_rest.model.Item;
 import com.udemy_rest.spring_rest.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,14 +12,33 @@ import java.util.List;
 public class ItemController {
     @Autowired
     private ItemService itemService;
+
     @GetMapping(value = "/items")
-    public List<Item> getAllItems(){
+    public List<Item> getAllItems() {
         return itemService.getAllItems();
     }
 
     @GetMapping(value = "/items/{itemId}")
-    public Item getItem(@PathVariable("itemId") String itemId){
+    public Item getItem(@PathVariable("itemId") String itemId) {
         return itemService.getItem(itemId);
+    }
+
+    @PostMapping(value = "/items")
+    public void addItem(@RequestBody Item item) {
+        itemService.addItem(item);
+    }
+
+    @PutMapping(value = "/items/{itemId}")
+    public void updateItem(
+            @RequestBody Item item,
+            @PathVariable("itemId") String itemId
+    ) {
+        itemService.updateItem(itemId, item);
+    }
+
+    @DeleteMapping(value = "/items/{itemId}")
+    public void deleteItem(@PathVariable("itemId") String itemId) {
+        itemService.deleteItem(itemId);
     }
 
 }
--- spring_rest/src/main/java/com/udemy_rest/spring_rest/service/ItemService.java
+++ spring_rest/src/main/java/com/udemy_rest/spring_rest/service/ItemService.java
@@ -3,18 +3,19 @@ package com.udemy_rest.spring_rest.service;
 import com.udemy_rest.spring_rest.model.Item;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ItemService {
-    private List<Item> allItems = Arrays.asList(
+    private List<Item> allItems = new ArrayList<> (Arrays.asList(
             new Item("10001", "ネックレス", "ジュエリー"),
             new Item("10002", "パーカー", "ファッション"),
             new Item("10003", "フェイスクリーム", "ビューティー"),
             new Item("10004", "サプリメント", "ヘルス"),
             new Item("10005", "ブルーベリー", "フード")
-    );
+    ));
 
     public List<Item> getAllItems(){
         return allItems;
@@ -27,6 +28,25 @@ public class ItemService {
             }
         }
         return null;
+    }
+
+
+    public void addItem(Item item){
+        allItems.add(item);
+    }
+
+    public void updateItem(String itemId, Item item){
+        for(int i = 0; i < allItems.size(); i++){
+            if(allItems.get(i).getItemId().equals(itemId)){
+                allItems.get(i).setItemName(item.getItemName());
+                allItems.get(i).setItemCategory(item.getItemCategory());
+                return;
+            }
+        }
+    }
+
+    public void deleteItem(String itemId){
+        allItems.removeIf(i -> i.getItemId().equals(itemId));
     }
 
 }
